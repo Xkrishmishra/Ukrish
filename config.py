edef39b2535a34e8dc13ad77b2bcb5800c1b336f
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <163736172+Xkrishmishra@users.noreply.github.com>
2024-10-27 20:31:47 +0530

    YTM

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ load_dotenv()
 
 API_ID = int(
     getenv(
-        "API_ID", "6435225")
+        "API_ID", "23270941")
 )
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+API_HASH = getenv("API_HASH", "307571de99aa4e7f108e9a21932ec8ac")
 
 STRING_SESSION = getenv("STRING_SESSION", "BQBiMZkAencMRi7_jY7wYBODdc_nEj0bjM-SIBUFTJIGrhzBqB3SCdzPxVZ6aAue5WCQeHc1AbH7Ipz0jFG4kIQWTL_jDA_6rvRcGNAzZV0esNvGWozY6FfEUPT2WCoMQ-z_kGw3wPwiiV-_VwqCQOzE7vQ6HKd0O7dj8ZLemMdS4NomdlWCETrYiuexiLKna1Yo_U3VZ3jNWGgCNRqBtWNfLg0I1Hnq9j2YubTj7y_I2vJQd_rUtc4l1GofrGbaFetB1CCbpiv16Q0S0At1w1jvDkZBS1pc4jnnAhh40XeFHuYgrhZD9i2sAoqVYsRej8dSiV9XFIfUgAAAAGPYj5JAA")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "7517701478:AAGSNXJOAvtFty0vARA38m34")
 
-LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002416571936)
+LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002359358037)
 
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "7176027733 7457599092 6124376040 1393205474").split())
+    map(int, getenv("OWNER_ID", "7458057585").split())
 )
 
 PREFIX = list(map(str, getenv("PREFIX", "" ".").split()))
